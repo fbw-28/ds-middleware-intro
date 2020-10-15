ebfca81b25fa45ce713a6c3cca3ae31880ac5c91
LenaIT <olena.denysova@gmail.com>
2020-10-15 11:19:16 +0200

    witout bonus

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
+
+app.use( morgan('dev') );
+
+app.use(express.json());
 
 app.get('/getme', (req, res) => {
   res.json({})
 });
 
-app.post('/postme', (req, res) => {
-  res.json({})
-})
+
+const checkBody = (req, res, next) => {
+  console.log(req.body);
+  next();
+}
+
+app.post('/postme',checkBody, (req, res) => {
+  res.json({message: "Hello ckeckBody"})
+});
+
 
 app.listen(5000, () => {
   console.log('Example app listening on port 5000!');
