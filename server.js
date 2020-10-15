a1c3cc97532e2efe2862aeb97ae91ea0977aed8f
Filippo <filippo.moretti6@gmail.com>
2020-10-15 13:47:25 +0200

    done

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 
+//Install express and morgan please (in one go: `npm i express morgan`)
+const morgan = require("morgan")
+
+app.use( express.json() ) // JSON body parser middleware into req.body (oject)
+
+app.use( morgan('dev') ) // setup morgan request logger middleware
+
+/** MIDDLEWARE (generic) run one every route*/
+ app.use( (req, res, next) => {
+   let isbodyEmpty=Object.keys(req.body).length>0 //how to check if the body is empty
+   if (isbodyEmpty){
+    console.log("body:", req.body)
+   }
+  
+  next()
+ })
+
+
+
 app.get('/getme', (req, res) => {
-  res.json({})
+  res.json({message: 'get me'})
 });
 
 app.post('/postme', (req, res) => {
-  res.json({})
+  res.json({message: 'post me'})
 })
 
 app.listen(5000, () => {
