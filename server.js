74989e1b34b63c857c24bce423cb4bb4fbe1ac06
losrobbos <losrobbos@web.de>
2020-10-15 11:45:04 +0200

    Added solution (without bonis)

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan")
+
+app.use( morgan('dev') ) // register morgan middleware!
+
+app.use( express.json() ) // => parse incoming JSON string into req.body (object)
+
+/** MIDDLEWARE (generic)  => runs on every call to any route
+ * 
+ * Logs for me the body, if one exists
+*/
+app.use( (req, res, next) => {
+  let isBodyEmpty = Object.keys( req.body ).length > 0
+
+  if(isBodyEmpty) {
+    console.log("[Middleware] Body scan: ",  req.body)
+  }
+  next()
+})
 
 app.get('/getme', (req, res) => {
-  res.json({})
+  res.json({ message: "Hello" })
 });
 
 app.post('/postme', (req, res) => {
-  res.json({})
+  res.json(req.body)
 })
 
 app.listen(5000, () => {
