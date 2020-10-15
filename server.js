01e5a819e8d94fa1114cdc1ebf1c1ad274911323
jsk900 <jsk900@yahoo.co.uk>
2020-10-15 10:39:57 +0200

    completed

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
+app.use(express.json())
+
+const logBody = (req, res, next) => {
+  console.log(req.body)
+  next()
+}
+
+const fullName = (req, res, next) => {
+  if (!req.body.firstName && !req.body.lastName) return (res.json({message: 'oy div, send me proper data'}))
+  const fullName = `${req.body.firstName} ${req.body.lastName}`
+  req.body.fullName = fullName
+  console.log(fullName)
+  next()
+}
 
 app.get('/getme', (req, res) => {
-  res.json({})
+  res.json({message: '/getme'})
 });
 
-app.post('/postme', (req, res) => {
-  res.json({})
+app.post('/postme', fullName, logBody, (req, res) => {
+  res.json({message: '/postme'})
 })
 
 app.listen(5000, () => {
   console.log('Example app listening on port 5000!');
 });
 
-//Run app, then load http://localhost:5000 in a browser to see the output.
